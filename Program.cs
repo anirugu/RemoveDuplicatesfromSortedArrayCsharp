e1588a04e6a24d7d28f3291b3dfb110dee41984c
Anirudha Gupta <35772421+anirugu@users.noreply.github.com>
2023-01-11 10:01:59 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.InteropServices.JavaScript;
+
+namespace RemoveDuplicatesfromSortedArrayCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+public class Solution
+{
+    public int RemoveDuplicates(int[] nums)
+    {
+       for(int i=0; i<nums.Length; i++)
+        {
+            while(i < nums.Length && )
+        }
+    }
+}
+// Find the every next item and set it if it's the same.
